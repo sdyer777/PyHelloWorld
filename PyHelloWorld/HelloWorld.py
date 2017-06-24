8aeb15e72e4bf73f4c29091aeae6774b07a9d770
sdyer777 <sdyer777@yahoo.com>
2017-06-24 14:57:54 -0400

    First actual Python script added.

---
+++ PyHelloWorld/HelloWorld.py
@@ -0,0 +1,10 @@
+'''
+Created on Jun 24, 2017
+
+@author: stevendyer
+'''
+def hello(myInput):
+    return "Hello World: " + myInput  + " tester"
+
+myInput = input("Please type your name: ") 
+print(hello(myInput))
